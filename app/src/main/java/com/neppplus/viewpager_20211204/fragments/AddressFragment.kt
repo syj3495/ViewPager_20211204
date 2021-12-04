17f46e248fe8bfbb69b25ace378cee329a48985a
yeonjin seong <syj3405@naver.com>
2021-12-04 12:46:33 +0900

    거주지 보여주는 프래그먼트 추가 (복사 붙여넣기)

---
+++ app/src/main/java/com/neppplus/viewpager_20211204/fragments/AddressFragment.kt
@@ -0,0 +1,19 @@
+package com.neppplus.viewpager_20211204.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.neppplus.viewpager_20211204.R
+
+
+class AddressFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_address, container, false)
+    }
+}
