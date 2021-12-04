1e14b23bb980c42d2d8e342fe27103da3777255a
yeonjin seong <syj3405@naver.com>
2021-12-04 12:48:23 +0900

    3장의 프래그먼트 완성

---
+++ app/src/main/java/com/neppplus/viewpager_20211204/fragments/AgeFragment.kt
@@ -0,0 +1,19 @@
+package com.neppplus.viewpager_20211204.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.neppplus.viewpager_20211204.R
+
+
+class AgeFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_age, container, false)
+    }
+}
